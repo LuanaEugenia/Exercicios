4df82536d02af01adc707061787ffe41677f989f
Luana Eugênia <luaeugenia1309@outlook.com>
2020-10-14 12:16:27 -0300

    Exercício 7 - mostrar números impares em determinado intervalo de números

---
+++ src/NumerosImpares.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class NumerosImpares {
+    public static void main(String[] args) {
+        NumerosImpares numerosImpares = new NumerosImpares();
+        numerosImpares.obterEntradaUsuario();
+    }
+
+    public void obterEntradaUsuario() {
+        final int SIM = 1;
+        final int NAO = 2;
+        Scanner leia = new Scanner(System.in);
+        int inicioIntervalo;
+        int fimIntervalo;
+        int repetirNovamente = NAO;
+
+        do {
+            System.out.print("Informe o ínico do programa: ");
+            inicioIntervalo = leia.nextInt();
+            System.out.print("Informe o final do programa: ");
+            fimIntervalo = leia.nextInt();
+            System.out.println("ºººººººººººººººººººººººººººººººººººººººººººººººººººººººººººº");
+
+            for (int i = inicioIntervalo; i <= fimIntervalo; i++) {
+                if (NumerosImpares.verificarImpar(i)) {
+                    System.out.println(i);
+                }
+            }
+
+            for (int i = inicioIntervalo; i >= fimIntervalo; i--) {
+                if (NumerosImpares.verificarImpar(i)) {
+                    System.out.println(i);
+                }
+            }
+            System.out.println("ºººººººººººººººººººººººººººººººººººººººººººººººººººººººººººº");
+            do {
+                System.out.print("\nDeseja verificar novamente: Digite: " + SIM + "/SIM ou " + NAO + "/ NÃO: ");
+                repetirNovamente = leia.nextInt();
+                if (repetirNovamente != SIM && repetirNovamente != NAO) {
+                    System.out.println("\nºººººººººººººººººººººººººººººººººººººººººººººººººººººººººººº");
+                    System.out.println("Experimente digitar a resposta CORRETA!");
+                    System.out.println("ºººººººººººººººººººººººººººººººººººººººººººººººººººººººººººº");
+                }
+            } while (repetirNovamente != SIM && repetirNovamente != NAO);
+        } while (repetirNovamente == SIM);
+
+        System.out.println("\n Saindo...");
+    }
+
+    public static boolean verificarImpar(int numero) {
+        return numero % 2 != 0;
+    }
+}
+//- Faça um programa que imprima na tela apenas os números ímpares entre
+// um intervalo determinado pelo usuário.
+// Ex: 1-10 > 1 3 5 7 9; 10-1 > 9 7 5 3 1
