4a8d8e33107f0abd649790c465a63345e5ae97f6
Luana Eugênia <luaeugenia1309@outlook.com>
2020-10-18 23:03:06 -0300

    Exercício 10 - Obter quantidade de alunos com média maior ou igual a 7.

---
+++ src/MediaAluno.java
@@ -0,0 +1,62 @@
+import java.text.DecimalFormat;
+import java.util.Random;
+import java.util.Scanner;
+
+public class MediaAluno {
+    public static void main(String[] args) {
+        new MediaAluno().entradaUsuario();
+    }
+
+    public void entradaUsuario() {
+        final int SIM = 1;
+        final int NAO = 2;
+        DecimalFormat decimalFormat = new DecimalFormat("#0.0");
+        Scanner leia = new Scanner(System.in);
+        double[][] alunos = new double[10][4];
+        double[] medias = new double[10];
+        int executarNovamente = NAO;
+
+        for (int i = 0; i < alunos.length; i++) {
+            for (int j = 0; j < alunos[i].length; j++) {
+                System.out.print("Digite a " + (j + 1) + "° nota do " + (i + 1) + "° aluno: ");
+                alunos[i][j] = new Random().nextDouble() * 10;
+                System.out.println(decimalFormat.format(alunos[i][j]));
+            }
+
+            medias[i] = MediaAluno.calcularMedia(alunos[i]);
+            System.out.println("°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°");
+        }
+
+        System.out.println(MediaAluno.contarNotasMaioresQue6(medias) + " alunos tem média maior ou igual a 7.");
+        System.out.println(alunos.length - MediaAluno.contarNotasMaioresQue6(medias) + " alunos não tem média maior ou igual a 7.");
+    }
+
+    public static double somarVetor(double[] numeros) {
+        double soma = 0.0;
+
+        for (double numero : numeros) {
+            soma += numero;
+        }
+
+        return soma;
+    }
+
+    public static double calcularMedia(double[] numeros) {
+        return MediaAluno.somarVetor(numeros) / numeros.length;
+    }
+
+    public static int contarNotasMaioresQue6(double[] numeros) {
+        int quantidadeNumerosMaioresQue6 = 0;
+
+        for (double numero : numeros) {
+            if (numero >= 7) {
+                quantidadeNumerosMaioresQue6++;
+            }
+        }
+
+        return quantidadeNumerosMaioresQue6;
+    }
+}
+
+// - Faça um Programa que peça as quatro notas de 10 alunos, calcule e armazene num vetor a média de cada aluno,
+// imprima o número de alunos com média maior ou igual a 7,0.
